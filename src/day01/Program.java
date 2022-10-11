e9e34480b0757a934a8fba17ccad3ed6c965fed9
ademgencer <ademyasargencer@gmail.com>
2022-10-11 21:38:41 +0300

    Adem Gencer değişiklik yapıldı.

--- src/day01/Program.java
+++ src/day01/Program.java
@@ -3,5 +3,7 @@ package day01;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Merhaba!");
+
+        System.out.println("Deneme yapıyoruz..");
     }
 }
